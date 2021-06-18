dd172fa7f8a89b0e9787f0180d72ae30a10a51b9
zeeuoo <45193893+zeeuoo@users.noreply.github.com>
2021-06-18 10:32:30 +0900

    최초버전

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    $("#privacyModal").hide(); //처음에 숨김
+    
+        //모달팝업
+        var posY;
+        $('a[href="#privacyModal"]').click(function(event) {
+            event.preventDefault();
+            if($(this).attr("rel") == "modal:open"){
+                if($(window).width() < 1024){
+                    posY = $(window).scrollTop();
+                    $(".wrap").css("top", -posY); 
+                }
+                $("#privacyModal").fadeIn();      
+                $("html, body").addClass("not_scroll");
+                
+            } else if($(this).attr("rel") == "modal:close"){
+                $("#privacyModal").fadeOut();  
+                $("html, body").removeClass("not_scroll");
+                posY = $(window).scrollTop();
+            }
+        });
+
+        //팝업창 높이 계산
+        var modalH = $(".modalWrap").height();
+        var titleH = $(".modalWrap h3").height();
+        if($(window).width() < 630){
+            modalH = $(window).height();
+        }
+        $(".popupContWrap").css("height", modalH - titleH);
+    
+});
